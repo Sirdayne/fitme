548dc8cf164c4665d3fdb4e7edb67c11f53c9386
Dayne <mussinyernar@gmail.com>
2017-03-30 15:54:39 +0600

    ENDED VERSION

--- js/fitme.js
+++ js/fitme.js
@@ -23,6 +23,14 @@ $(document).ready(function(){
         $('.navigations').toggleClass('navigations-active');
     });
     
+    $('body').on('click','.js-cabinet' , function(){
+        $('.modal-wrapper').addClass('js-active');
+    });
+    
+    $('body').on('click','.modal-close' , function(){
+        $('.modal-wrapper').removeClass('js-active');
+    });
+    
     //PARALLAX
     if ($(window).width() > 1000) {
         var scene = document.getElementById('main');
@@ -81,6 +89,35 @@ $(document).ready(function(){
                 });
             }
 
+        });
+    
+         /* SLICK SLIDERS */
+
+        $('#slick-slider-0').slick({
+            centerMode: false,
+            slidesToShow: 1,
+            centerPadding: '0px',
+            arrows: true,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 4000,
+        });    
+
+        $('.navigations').InPageNav({
+            currentClass: 'nav-active',
+            scrollSpeed: 1000,
+            scrollOffset: 60,
+            scrollThreshold: 0.5,
+            filter: '',
+            easing: 'swing',
+            begin: function() {
+                },
+            end: function() {
+                //$('body').removeClass('menu-opened').addClass('menu-closed');
+            },
+            scrollChange: function($currentListItem) {
+                    // Fired when you enter a section and pass the list item of the section
+                }
         });
     
 });
